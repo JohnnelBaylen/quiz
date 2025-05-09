496b6ea84aeb88b59268fe22db7814f3d1f2967a
jnelbaylen <jnelbaylen@gmail.com>
2025-05-09 10:38:31 +0800

    chore: update data

---
+++ src/data/data.js
@@ -0,0 +1,91 @@
+export const data = [
+    {
+        question: "What color is the sky on a clear day?",
+        option1: "Red",
+        option2: "Blue",
+        option3: "Green",
+        option4: "Yellow",
+        answer: 2,
+    },
+
+    {
+        question: "How many days are there in a week?",
+        option1: "5",
+        option2: "6",
+        option3: "7",
+        option4: "8",
+        answer: 3,
+    },
+
+    {
+        question: "What is the capital of the Philippines?",
+        option1: "Cebu",
+        option2: "Davao",
+        option3: "Manila",
+        option4: "Quezen",
+        answer: 3,
+    },
+
+    {
+        question: "Which animal says “meow”?",
+        option1: "Dog",
+        option2: "Cow",
+        option3: "Cat",
+        option4: "Bird",
+        answer: 3,
+    },
+
+    {
+        question: "What is 5 + 5?",
+        option1: "5",
+        option2: "10",
+        option3: "15",
+        option4: "20",
+        answer: 2,
+    },
+
+    {
+        question: "Which one is a fruit?",
+        option1: "Carrot",
+        option2: "Lettuce",
+        option3: "Apple",
+        option4: "Broccoli",
+        answer: 3,
+    },
+
+    {
+        question: "What do we use to write on paper?",
+        option1: "Spoon",
+        option2: "Pencil",
+        option3: "Eraser",
+        option4: "Scissors",
+        answer: 2,
+    },
+
+    {
+        question: "Which season is the coldest?",
+        option1: "Summer",
+        option2: "Spring",
+        option3: "Autumn",
+        option4: "Winter",
+        answer: 4,
+    },
+
+    {
+        question: "How many legs does a spider have?",
+        option1: "4",
+        option2: "6",
+        option3: "8",
+        option4: "10",
+        answer: 3,
+    },
+
+    {
+        question: "What do you drink when you're thirsty?",
+        option1: "Oil",
+        option2: "Water",
+        option3: "Vinegar",
+        option4: "Salt",
+        answer: 2,
+    },
+];
